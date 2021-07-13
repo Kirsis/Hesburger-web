c52f81ae6adbe4740e5d3bd8cc90d96876f29d12
Kirsis <87366342+Kirsis@users.noreply.github.com>
2021-07-13 12:58:13 +0300

    Delete html directory

--- html/bar.php
+++
@@ -1,9 +0,0 @@
-
-<form action="" method="post">
-<input type="text" name="search" required
-oninvalid="this.setCustomValidity('Ievadiet vardu')"
-    oninput="setCustomValidity('')">
-<input type="submit" name="submit" value="Atrast">
-<?php
-include 'em.php';
-?>
